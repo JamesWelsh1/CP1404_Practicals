93e73f6a98e3251cece0f8fa7ae8bccdfaa9e5b5
JamesWelsh1 <james.welsh@my.jcu.edu.au>
2018-04-08 10:13:25 +1000

    update temeratures program with functions

--- Prac_03/temperatures.py
+++ Prac_03/temperatures.py
@@ -2,25 +2,41 @@
 CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
+MENU = """
+    C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        fahrenheit = float(input("fahrenheit: "))
-        celsius = (fahrenheit -32)/9.0 *5
-        print("Result: {:.2f} C".format(celsius))
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-    else:
-        print("Invalid option")
+
+def main():
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            fahrenheit = convert_celsius_to_fahrenheit()
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            celsius = convert_fahrenheit_to_celsius()
+            print("Result: {:.2f} C".format(celsius))
+            # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+            # Remove the "pass" statement when you are done. It's a placeholder.
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_fahrenheit_to_celsius():
+    fahrenheit = float(input("fahrenheit: "))
+    celsius = (fahrenheit - 32) / 9.0 * 5
+    return celsius
+
+
+def convert_celsius_to_fahrenheit():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+main()
