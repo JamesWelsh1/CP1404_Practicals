b420c5f1e9d66b3039fec12e0fea4f6c23b069e8
JamesWelsh1 <james.welsh@my.jcu.edu.au>
2018-05-08 11:47:50 +1000

    update use os.walk on all files in 'Lyrics' folder

--- Prac_09/cleanup_files.py
+++ Prac_09/cleanup_files.py
@@ -20,33 +20,34 @@ def main():
         os.mkdir('temp')
     except FileExistsError:
         pass
-    # loop through each file in the (original) directory
-    for filename in os.listdir('.'):
-        # ignore directories, just process files
-        if not os.path.isdir(filename):
-            new_name = get_fixed_filename(filename)
-            print(new_name)
 
-            # NOTE: These options won't just work...
-            # they show you ways of renaming and moving files,
-            # but you need to have valid filenames. You can't make a file with
-            # a blank name, and on Windows you can't have files with the same
-            # characters but different case (e.g. a.TXT and A.txt)
-            # So, you need to get valid filenames before you can use these.
 
-            # Option 1: rename file to new name - in place
-            # os.rename(filename, new_name)
+    os.chdir('..')  # .. means "up" one directory
+    print(os.getcwd())
+    for dir_name, subdir_list, file_list in os.walk('.'):
+        print("In", dir_name)
+        print("\tcontains subdirectories:", subdir_list)
+        print("\tand files:", file_list)
+        # loop through each file in the (original) directory
+        for filename in file_list:
+            # ignore directories, just process files
+            if not os.path.isdir(filename):
+                new_name = get_fixed_filename(filename)
+                print(new_name)
 
-            # Option 2: move file to new place, with new name
-            shutil.move(filename, 'temp/' + new_name)
+                # NOTE: These options won't just work...
+                # they show you ways of renaming and moving files,
+                # but you need to have valid filenames. You can't make a file with
+                # a blank name, and on Windows you can't have files with the same
+                # characters but different case (e.g. a.TXT and A.txt)
+                # So, you need to get valid filenames before you can use these.
 
-            # Processing subdirectories using os.walk()
+                # Option 1: rename file to new name - in place
+                # os.rename(filename, new_name)
 
-            # os.chdir('..')  # .. means "up" one directory
-            # for dir_name, subdir_list, file_list in os.walk('.'):
-            #     print("In", dir_name)
-            #     print("\tcontains subdirectories:", subdir_list)
-            #     print("\tand files:", file_list)
+                # Option 2: move file to new place, with new name
+                # shutil.copy(filename, 'temp/' + new_name)
+
 
 
 def get_fixed_filename(filename):
